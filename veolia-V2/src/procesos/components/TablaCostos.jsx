7181791b2b2ea72e845e41e86a1ee4533f7aa600
Pancho Polanco <cmtovar@unimayor.edu.co>
2024-09-04 18:38:00 -0500

    se agrego la barra de porcentaje a la tabla de clus

--- veolia-V2/src/procesos/components/TablaCostos.jsx
+++ veolia-V2/src/procesos/components/TablaCostos.jsx
@@ -1,4 +1,41 @@
  export const TablaCostos = ({data}) => {
+
+    const onPorcentaje = (value) => {
+        if (value.includes('%')) {
+            const percentage = parseFloat(value.replace('%', '').replace(',', '.'));
+            const clampedPercentage = Math.max(0, Math.min(100, percentage));
+            return (
+                <div className="progress-bar-container">
+                    <span className="progress-bar-text">
+                            {`${clampedPercentage}%`}
+                    </span>
+                    <div
+                        className="progress-bar"
+                        style={{ width: `${clampedPercentage}%` }}
+                    >
+                    </div>
+                </div>
+            );
+        } else {
+            return <div>
+                {!isNaN(value.replace(",", "."))
+                    ? formateoMiles(value)
+                    : value}
+            </div>
+        }
+    }
+
+    const isNumericType = (value) => {
+        return !isNaN(value.replace(',', '.')) || (value.includes("%")) ? "NumericValue" : "StringValue"
+    }
+
+    const formateoMiles = (numeroSinFormatear) => {
+      let arrayNumeros = numeroSinFormatear.replace(',', '.').split('.')
+      let enteroFormateado = arrayNumeros[0]
+      enteroFormateado =  new Intl.NumberFormat('es-MX').format(Number(enteroFormateado))
+      return arrayNumeros[1] ? `${enteroFormateado}.${arrayNumeros[1]}`: `${enteroFormateado}`
+    }
+
     return(
     <>
         <div>
@@ -21,11 +58,31 @@
                             </tr>
                         </thead>
                         <tbody className="tbodyCosto">
+                        {/* {data &&
+                            data.data.map((item, index) => (
+                                <tr key={index}>
+                                {item.map((value, idx) => (
+                                    <td 
+                                        key={idx}
+                                    >
+                                        {value}
+                                    </td>
+                                ))}
+                                </tr>
+                            ))
+                        } */}
                         {data &&
                             data.data.map((item, index) => (
                                 <tr key={index}>
                                 {item.map((value, idx) => (
-                                    <td key={idx}>{value}</td>
+                                    <td 
+                                        key={idx}
+                                        className={isNumericType(value)}
+                                    >
+                                        {
+                                            onPorcentaje(value)
+                                        }
+                                    </td>
                                 ))}
                                 </tr>
                             ))
