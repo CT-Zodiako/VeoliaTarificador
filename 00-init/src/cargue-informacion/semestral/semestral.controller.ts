9477dbf6cfc93c9d479e0aede90cb1072f77e84b
Zodiako <cristianjvz98@gmail.com>
2024-09-21 00:32:13 -0500

    Cargue mensual con todos sus endpoints

--- 00-init/src/cargue-informacion/semestral/semestral.controller.ts
+++ 00-init/src/cargue-informacion/semestral/semestral.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { SemestralService } from './semestral.service';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
 
 @Controller('semestral')
 export class SemestralController {
@@ -10,7 +11,30 @@ export class SemestralController {
     return this.semestralService.consultaPropias(data);
   }
 
-  
+  @Post('carguePropia')
+  @UseGuards()
+  carguePropia(@Query() data, @GetUser() usuario) {
+    return this.semestralService.carguePropia(data,usuario.SISU_ID);
+  }
+
+  @Post('cargueInfCompetidor')
+  @UseGuards()
+  cargueInfCompetidor(@Query() data, @GetUser() usuario) {
+    return this.semestralService.cargueInfCompetidor(data,usuario.SISU_ID);
+  }
+
+  @Post('cargueUsuarios')
+  @UseGuards()
+  cargueUsuarios(@Query() data, @GetUser() usuario) {
+    return this.semestralService.cargueUsuarios(data,usuario.SISU_ID);
+  }
+
+  @Post('cargueTerceros')
+  @UseGuards()
+  cargueTerceros(@Query() data, @GetUser() usuario) {
+    return this.semestralService.cargueTerceros(data,usuario.SISU_ID);
+  }
+
 
 
 }
