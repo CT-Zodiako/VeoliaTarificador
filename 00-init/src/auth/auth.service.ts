20beca7647ff21f77c25426903979ebaf265848c
Zodiako <cristianjvz98@gmail.com>
2024-10-31 15:50:22 -0500

    Fix en Asignar menu sumando los arreglos

--- 00-init/src/auth/auth.service.ts
+++ 00-init/src/auth/auth.service.ts
@@ -282,12 +282,14 @@ export class AuthService {
     try {
       const { sisuId,  opcionesSinAsignar, opcionesAsignada} = body;
 
+      const eliminarTodos: number[] = [...opcionesSinAsignar, ...opcionesAsignada];
+
       const sqlDesmarcar = `
       DELETE FROM 
         TARIFICADOR.AUGE_USUAMENU
       WHERE SISU_ID = :1 AND MENU_ID = :2`;
 
-      for (const opcion of opcionesSinAsignar) {
+      for (const opcion of eliminarTodos) {
       await this.menuUserRepository.query(sqlDesmarcar, [sisuId, opcion]);
       }
       const sqlUpdt = `
